02e81f40181ad368008d2af9c0c277fe0fa89ce6
github username <github email address>
2018-08-21 23:26:10 -0500

    added sign in page and session

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, :recipes, :tags, :recipe_tags, :comments
+  resources :users, :recipes, :tags, :recipe_tags, :comments, :sessions
 end
